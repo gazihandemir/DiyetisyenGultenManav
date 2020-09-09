3bb434ac9dd00197ec4a3cbb01821a2662a28284
gazihandemir <gazihand@gmail.com>
2020-09-09 15:06:38 +0300

    09.09.2020 DatabaseContext oluşturuldu.

---
+++ DiyetisyenGultenManav.DataAccessLayer/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using DiyetisyenGultenManav.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiyetisyenGultenManav.DataAccessLayer
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Kullanıcı> Kullanıcılar { get; set; }
+        public DbSet<BlogYazısı> BlogYazıları { get; set; }
+        public DbSet<Yorum> Yorumlar { get; set; }
+        public DbSet<Kategori> Kategoriler { get; set; }
+        public DbSet<Diet> Dietler { get; set; }
+
+    }
+}
