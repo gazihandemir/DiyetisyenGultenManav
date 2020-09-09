328aa2ffd045329593efb3697332dfa420705b06
gazihandemir <gazihand@gmail.com>
2020-09-09 17:17:44 +0300

    09.09.2020 MyInitializer eklendi. FakeData ile kategori eklendi.

---
+++ DiyetisyenGultenManav.DataAccessLayer/MyInitializer.cs
@@ -0,0 +1,67 @@
+﻿using DiyetisyenGultenManav.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiyetisyenGultenManav.DataAccessLayer
+{
+    public class MyInitializer : CreateDatabaseIfNotExists<DatabaseContext>
+    {
+        protected override void Seed(DatabaseContext context)
+        {
+            Kullanıcı gazi = new Kullanıcı()
+            {
+                Name = "gazi",
+                Surname = "demir",
+                Email = "gazihand@gmail.com",
+                ActivateGuid = Guid.NewGuid(),
+                IsActive = true,
+                IsAdmin = true,
+                IsNormal = false,
+                IsOnline = false,
+                IsPayOnline = false,
+                Username = "gazihandemir",
+                Password = "123",
+                CreatedOn = DateTime.Now,
+                ModifiedOn = DateTime.Now.AddMinutes(5),
+                ModifiedUsername = "gazihandemir"
+            };
+            Kullanıcı gulten = new Kullanıcı()
+            {
+                Name = "gulten",
+                Surname = "manav",
+                Email = "gultenmanav@gmail.com",
+                ActivateGuid = Guid.NewGuid(),
+                IsActive = true,
+                IsAdmin = false,
+                IsNormal = false,
+                IsOnline = false,
+                IsPayOnline = false,
+                Username = "gultenmanav",
+                Password = "123",
+                CreatedOn = DateTime.Now,
+                ModifiedOn = DateTime.Now.AddMinutes(5),
+                ModifiedUsername = "gazihandemir"
+            };
+            context.Kullanıcılar.Add(gazi);
+            context.Kullanıcılar.Add(gulten);
+            context.SaveChanges();
+            // FakeDate ile kategori ekleme
+            for (int i = 0; i < 10; i++)
+            {
+                Kategori kat = new Kategori()
+                {
+                    Title = FakeData.PlaceData.GetStreetName(),
+                    Description = FakeData.PlaceData.GetAddress(),
+                   CreatedOn = DateTime.Now,
+                   ModifiedOn = DateTime.Now,
+                   ModifiedUsername = "gazihandemir"
+                };
+            }
+
+        }
+    }
+}
