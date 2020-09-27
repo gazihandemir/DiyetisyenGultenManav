8dde5f3843ee4a7c2cd5c04c16456176f2ff0ec3
gazihandemir <gazihand@gmail.com>
2020-09-27 10:01:20 +0300

    27.09.2020 NotifyViewModel ve ondan türüyen şeyler oluşturuldu.

---
+++ DiyetisyenGultenManav.WebApp/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DiyetisyenGultenManav.Entities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiyetisyenGultenManav.WebApp.ViewModels
+{
+    public class ErrorViewModel : NotifyViewModelBase<ErrorMessageObj>
+    {
+        public ErrorViewModel()
+        {
+            Title = "Geçersiz işlem !";
+        }
+
+    }
+}
---
+++ DiyetisyenGultenManav.WebApp/ViewModels/NotifyViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiyetisyenGultenManav.WebApp.ViewModels
+{
+    public class NotifyViewModelBase<T>
+    {
+        public List<T> Items { get; set; }
+        public string Header { get; set; }
+        public string Title { get; set; }
+        public bool IsRedirecting { get; set; }
+        public string RedirectingUrl { get; set; }
+        public int RedirectingTimeout { get; set; }
+        public NotifyViewModelBase()
+        {
+            Header = "Yönlendiriliyorsunuz."; // 
+            Title = "işlemi yazılımcı seçecek"; // Başlıkta ne yazsın.
+            IsRedirecting = true; // Yönlendirme olsun mu 
+            RedirectingUrl = "/Home/Index"; // Nereye yönlendirilsin
+            RedirectingTimeout = 8000; // kaç saniye sonra yönlendirilsin.
+        }
+    }
+}
