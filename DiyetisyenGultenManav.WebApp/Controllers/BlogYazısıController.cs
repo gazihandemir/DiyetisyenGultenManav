f618ae9d3f5e19fd01c59d636c944ec87f9465d2
gazihandemir <gazihand@gmail.com>
2020-10-05 12:15:46 +0300

    05.10.2020 CurrentSession ve Blog yazısı sayfaları oluşturuldu.

---
+++ DiyetisyenGultenManav.WebApp/Controllers/BlogYazısıController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DiyetisyenGultenManav.BusinessLayer;
+using DiyetisyenGultenManav.Entities;
+using DiyetisyenGultenManav.WebApp.Models;
+
+namespace DiyetisyenGultenManav.WebApp.Controllers
+{
+    public class BlogYazısıController : Controller
+    {
+        BlogYazısıManager blogYazısıManager = new BlogYazısıManager();
+        KategoriManager kategoriManager = new KategoriManager();
+        public ActionResult Index()
+        {
+            var blogYazısı = blogYazısıManager.ListQueryable().Include("Kategori")
+                .Where(x => x.Owner.Id == CurrentSession.User.Id).OrderByDescending(x => x.ModifiedOn);
+            return View(blogYazısı.ToList());
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogYazısı blogYazısı = blogYazısıManager.Find(x => x.Id == id);
+            if (blogYazısı == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogYazısı);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.KategoriId = new SelectList(kategoriManager.List(), "Id", "Title");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BlogYazısı blogYazısı)
+        {
+            if (ModelState.IsValid)
+            {
+                blogYazısıManager.Insert(blogYazısı);
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.KategoriId = new SelectList(kategoriManager.List(), "Id", "Title", blogYazısı.KategoriId);
+            return View(blogYazısı);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogYazısı blogYazısı = blogYazısıManager.Find(x => x.Id == id);
+            if (blogYazısı == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.KategoriId = new SelectList(kategoriManager.List(), "Id", "Title", blogYazısı.KategoriId);
+            return View(blogYazısı);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BlogYazısı blogYazısı)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blogYazısı).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.KategoriId = new SelectList(kategoriManager.List(), "Id", "Title", blogYazısı.KategoriId);
+            return View(blogYazısı);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogYazısı blogYazısı = blogYazısıManager.Find(x => x.Id == id);
+            if (blogYazısı == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogYazısı);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BlogYazısı blogYazısı = blogYazısıManager.Find(x => x.Id == id);
+            blogYazısıManager.Delete(blogYazısı);
+            return RedirectToAction("Index");
+        }
+    }
+}
