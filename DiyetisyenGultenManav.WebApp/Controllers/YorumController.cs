f6d46a967ad9d2c8fd8b95fd63a831e89ff6b6f8
gazihandemir <gazihand@gmail.com>
2020-11-26 10:59:16 +0300

    26.11.2020 ModalPopup dinamik veriler alındı

---
+++ DiyetisyenGultenManav.WebApp/Controllers/YorumController.cs
@@ -0,0 +1,30 @@
+﻿using DiyetisyenGultenManav.BusinessLayer;
+using DiyetisyenGultenManav.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiyetisyenGultenManav.WebApp.Controllers
+{
+    public class YorumController : Controller
+    {
+        private BlogYazısıManager blogYazısıManager = new BlogYazısıManager();
+        public ActionResult ShowNoteComments(int? id) 
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogYazısı blogYazısı = blogYazısıManager.ListQueryable().Include("Yorumlar").FirstOrDefault(x => x.Id == id); 
+            if (blogYazısı == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_PartialYorumlar",blogYazısı.Yorumlar);
+        }
+    }
+}
